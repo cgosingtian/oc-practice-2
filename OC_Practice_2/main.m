116862259e594c5fe8650a910c33885d60af1477
Chase Gosingtian <c.gosingtian@klab.com>
2014-07-15 16:17:50 +0800

    Initial Commit

---
+++ OC_Practice_2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  OC_Practice_2
+//
+//  Created by Chase Gosingtian on 7/15/14.
+//  Copyright (c) 2014 KLab Cyscorpions, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
