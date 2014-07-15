b3120037bf2b6f4e25e2b17871cca816848304f5
Chase Gosingtian <c.gosingtian@klab.com>
2014-07-15 16:52:30 +0800

    Student files (had ignored previously)

---
+++ OC_Practice_2/Student.h
@@ -0,0 +1,22 @@
+//
+//  Student.h
+//  OC_Practice_1
+//
+//  Created by Chase Gosingtian on 7/15/14.
+//  Copyright (c) 2014 KLab Cyscorpions, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+
+@property (nonatomic) int age;
+@property (nonatomic) NSMutableString *name;
+
+- (void)setName:(NSMutableString *)n;
+- (NSMutableString *)getName;
+
+- (void)setAge:(int)a;
+- (int)getAge;
+
+@end
