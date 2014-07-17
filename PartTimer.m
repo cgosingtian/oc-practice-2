97f2d15ce8556e73f13718c8f9eee54accdcdb08
Chase Gosingtian <c.gosingtian@klab.com>
2014-07-17 11:22:16 +0800

    Tested inheritance via new PartTimer object (extending Student).

---
+++ PartTimer.m
@@ -0,0 +1,15 @@
+//
+//  PartTimer.m
+//  OC_Practice_2
+//
+//  Created by Chase Gosingtian on 7/17/14.
+//  Copyright (c) 2014 KLab Cyscorpions, Inc. All rights reserved.
+//
+
+#import "PartTimer.h"
+
+@implementation PartTimer
+
+@synthesize job;
+
+@end
